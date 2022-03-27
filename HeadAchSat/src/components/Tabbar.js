a5df224d4eaa503b11db98fb21ab5c08181f45e7
mookmilks <siwaporn.promp@bumail.net>
2022-03-27 18:05:40 +0700

    Add Tabbar

--- HeadAchSat/src/components/Tabbar.js
+++ HeadAchSat/src/components/Tabbar.js
@@ -1,9 +1,20 @@
 import '../css/Tabbar.css';
+import { Link } from 'react-router-dom';
 
-export function Tabbar () {
+function Tabbar () {
   return (
-    <div className="tabbar">
-      <div className=".tabbar.stick-bottom"></div>
+    <div className="tabbar stick-bottom">
+      <Link to="/friend-list" className="tabbutton">
+        Friend List
+      </Link>
+      <Link to="/chat" className="tabbutton">
+        Chat
+      </Link>
+      <Link to="/profile" className="tabbutton">
+        Profile
+      </Link>
     </div>
   )
 }
+
+export default Tabbar;
